46deba981367cd491c71416e2006683d83cfc3e8
HumanGenerated <humanGenerated.TOBB@gmail.com>
2025-04-23 21:11:04 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -52,5 +52,5 @@ def shutdown_event():
     print("Spark session stopped")
 
 if __name__ == "__main__":
-     uvicorn.run("app.app:app", host="0.0.0.0", port=8000)
+     uvicorn.run("app:app", host="0.0.0.0", port=8000)
     
