d91ef02ae49defb1dd7030ab21ffdff621c53f2b
Nurşize Turan <98180991+nnursize@users.noreply.github.com>
2025-04-24 13:52:42 +0300

    Update spark_utils.py

--- app/spark_utils.py
+++ app/spark_utils.py
@@ -5,7 +5,7 @@ import os
 #python_path = r"C:\\Users\\USER\\Desktop\\Fuudiy\\Fuudiy-Backend\\fenv\\Scripts\\python.exe"
 #python_path = r"D:\OneDrive - TOBB Ekonomi ve Teknoloji Üniversitesi\Masaüstü\Fuudiy Back\venv\Scripts\python.exe"
 #python_path = r"/Users/berfinozcubuk/Desktop/tobb/24_25/fuudiy/.venv/bin/python"
-python_path = "/usr/bin/python3.9"
+python_path = "python3"
 
 os.environ['PYSPARK_PYTHON'] = python_path
 os.environ['PYSPARK_DRIVER_PYTHON'] = python_path
